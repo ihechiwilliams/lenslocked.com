6db41db17857dc2c3d1d8b1a1553c2ace6a32329
charles williams <charlesclinton2003@gmail.com>
2020-12-30 18:09:44 +0100

    setup ci/cd

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/charleyvibez/lenslocked.com
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeView *views.View
-var contactView *views.View
-var faqView *views.View
+var (
+	homeView    *views.View
+	contactView *views.View
+	faqView     *views.View
+	signupView	*views.View
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -27,6 +30,11 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	must(faqView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 // A helper function that panics on any error
 func must(err error) {
 	if err != nil {
@@ -38,10 +46,12 @@ func main() {
 	homeView = views.NewView("bootstrap","views/home.gohtml")
 	contactView = views.NewView("bootstrap","views/contact.gohtml")
 	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 	port := getPort()
 	http.ListenAndServe(port, r)
 }
